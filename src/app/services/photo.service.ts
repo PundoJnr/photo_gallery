68433259c76196ddc9c1b56f0bcc0172165ba7dc
PundoJnr <pundoarthur@gmail.com>
2020-03-06 08:23:19 +0300

    add filesystem api

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -14,17 +14,59 @@ export class PhotoService {
 
   public photos: Photo[] = [];
 
+  private async savePicture(cameraPhoto: CameraPhoto) {
+      //convert photo to base 64 format
+      const base64Data = await this.readAsBase64(cameraPhoto);
+
+      //write the file to the data directory
+      const fileName = new Date().getTime()+'.jpeg';
+      await Filesystem.writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: FilesystemDirectory.Data
+      });
+
+      return await this.getPhotoFile(cameraPhoto, fileName);
+   }
+
+   private async readAsBase64(cameraPhoto: CameraPhoto){
+     const response = await fetch(cameraPhoto.webPath);
+     const blob = await response.blob();
+
+     return await this.convertBlobToBase64(blob) as string;
+   }
+
+   convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject)=>{
+     const reader = new FileReader;
+     reader.onerror = reject;
+     reader.onload = () =>{
+       resolve(reader.result);
+     };
+     reader.readAsDataURL(blob);
+   });
+   private async getPhotoFile(cameraPhoto: CameraPhoto, fileName: String
+    ): Promise<Photo>{
+   return{
+     filepath: fileName,
+     webviewPath:cameraPhoto.webPath
+   };
+   }
   public async addNewToGallery() {
+    //take picture
     const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
+      resultType: CameraResultType.Uri,//file-based data
+      source: CameraSource.Camera,//automatically take a new photo with the camera
+      quality: 100 // photo quality is 0-100
     });
+        //save picture to photo collection
+        const savedImageFile = await this.savePicture(capturedPhoto);
+        this.photos.unshift(savedImageFile);
 
     this.photos.unshift({
       filepath: "soon...",
       webviewPath: capturedPhoto.webPath
     })
+
   }
   constructor() { }
 }
